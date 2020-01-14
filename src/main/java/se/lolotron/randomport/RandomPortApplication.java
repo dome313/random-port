376ac86e3e9dbfeb5c459461086cb454a0c5ebc8
Marko Vranjkovic <marko.vranjkovic@aerlingus.com>
2020-01-14 08:49:44 +0000

    random port app

---
+++ src/main/java/se/lolotron/randomport/RandomPortApplication.java
@@ -0,0 +1,13 @@
+package se.lolotron.randomport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomPortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomPortApplication.class, args);
+	}
+
+}
